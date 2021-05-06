21d753f612970b521688b3c4e06d13d30719da55
Zach Schroeder <zach@contactci.co>
2021-05-06 18:18:37 +0000

    Feature/editor scripts

---
+++ Assets/Maestro/Core/Editor/MaestroHandV2Editor.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Maestro
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(MaestroHandV2))]
+    public class MaestroHandV2Editor : Editor
+    {
+        private bool showHandConfig = false;
+        private string showHandConfigTxt = "Hand Definition";
+
+        private bool showAdvConfig = false;
+        private string showAdvConfigTxt = "Advanced Config";
+
+        private bool showDebug = false;
+        private string showDebugTxt = "Debug";
+
+        public void OnEnable()
+        {
+            /* Effectively the inspector's OnStart */
+        }
+
+        public override void OnInspectorGUI()
+        {
+            MaestroHandV2 hand = (MaestroHandV2)target;
+
+            EditorGUILayout.LabelField("Main Configuration", EditorStyles.boldLabel);
+            hand.whichHand = (WhichHand) EditorGUILayout.EnumPopup("Which Hand", hand.whichHand);
+            hand.otherHand = (MaestroHandV2)EditorGUILayout.ObjectField("Other Hand", hand.otherHand, typeof(MaestroHandV2), allowSceneObjects: true);
+            EditorGUILayout.Space();
+
+            bool handNotFullyDefined =
+                hand.ThumbTip == null || hand.ThumbMiddle == null || hand.ThumbKnuckle == null ||
+                hand.IndexTip == null || hand.IndexMiddle == null || hand.IndexKnuckle == null ||
+                hand.MiddleTip == null || hand.MiddleMiddle == null || hand.MiddleKnuckle == null ||
+                hand.RingTip == null || hand.RingMiddle == null || hand.RingKnuckle == null ||
+                hand.LittleTip == null || hand.LittleMiddle == null || hand.LittleKnuckle == null ||
+                hand.PalmBase == null;
+            
+            if (hand.otherHand == null) {
+                EditorGUILayout.HelpBox("Other hand not defined! Two-handed interactions will disabled!", MessageType.Warning, wide: true);
+            }
+
+            if (handNotFullyDefined) {
+                EditorGUILayout.HelpBox("Hand not fully defined!", MessageType.Error, wide: true);
+            }
+
+            // Read-only toggles for grab states
+            EditorGUILayout.Toggle("Is Grabbing", hand.grabbing);
+            EditorGUILayout.Toggle("Is Two-Hand Grabbing", hand.twoHandGrabbing);
+
+            showHandConfig = EditorGUILayout.Foldout(showHandConfig, showHandConfigTxt);
+            if (showHandConfig) {
+                EditorGUILayout.LabelField("Hand Sizing", EditorStyles.boldLabel);
+                hand.TipSize = EditorGUILayout.FloatField("Tip Size", hand.TipSize);
+                hand.MiddleSize = EditorGUILayout.FloatField("Middle Size", hand.MiddleSize);
+                hand.KnuckleSize = EditorGUILayout.FloatField("Knuckle Size", hand.KnuckleSize);
+                EditorGUILayout.Space();
+
+                EditorGUILayout.LabelField("Positions on Hand", EditorStyles.boldLabel);
+                hand.ThumbTip = (Transform)EditorGUILayout.ObjectField("Thumb Tip", hand.ThumbTip, typeof(Transform), allowSceneObjects: true);
+                hand.ThumbMiddle = (Transform)EditorGUILayout.ObjectField("Thumb Middle", hand.ThumbMiddle, typeof(Transform), allowSceneObjects: true);
+                hand.ThumbKnuckle = (Transform)EditorGUILayout.ObjectField("Thumb Knuckle", hand.ThumbKnuckle, typeof(Transform), allowSceneObjects: true);
+                EditorGUILayout.Space();
+                hand.IndexTip = (Transform)EditorGUILayout.ObjectField("Index Tip", hand.IndexTip, typeof(Transform), allowSceneObjects: true);
+                hand.IndexMiddle = (Transform)EditorGUILayout.ObjectField("Index Middle", hand.IndexMiddle, typeof(Transform), allowSceneObjects: true);
+                hand.IndexKnuckle = (Transform)EditorGUILayout.ObjectField("Index Knuckle", hand.IndexKnuckle, typeof(Transform), allowSceneObjects: true);
+                EditorGUILayout.Space();
+                hand.MiddleTip = (Transform)EditorGUILayout.ObjectField("Middle Tip", hand.MiddleTip, typeof(Transform), allowSceneObjects: true);
+                hand.MiddleMiddle = (Transform)EditorGUILayout.ObjectField("Middle Middle", hand.MiddleMiddle, typeof(Transform), allowSceneObjects: true);
+                hand.MiddleKnuckle = (Transform)EditorGUILayout.ObjectField("Middle Knuckle", hand.MiddleKnuckle, typeof(Transform), allowSceneObjects: true);
+                EditorGUILayout.Space();
+                hand.RingTip = (Transform)EditorGUILayout.ObjectField("Ring Tip", hand.RingTip, typeof(Transform), allowSceneObjects: true);
+                hand.RingMiddle = (Transform)EditorGUILayout.ObjectField("Ring Middle", hand.RingMiddle, typeof(Transform), allowSceneObjects: true);
+                hand.RingKnuckle = (Transform)EditorGUILayout.ObjectField("Ring Knuckle", hand.RingKnuckle, typeof(Transform), allowSceneObjects: true);
+                EditorGUILayout.Space();
+                hand.LittleTip = (Transform)EditorGUILayout.ObjectField("Little Tip", hand.LittleTip, typeof(Transform), allowSceneObjects: true);
+                hand.LittleMiddle = (Transform)EditorGUILayout.ObjectField("Little Middle", hand.LittleMiddle, typeof(Transform), allowSceneObjects: true);
+                hand.LittleKnuckle = (Transform)EditorGUILayout.ObjectField("Little Knuckle", hand.LittleKnuckle, typeof(Transform), allowSceneObjects: true);
+                EditorGUILayout.Space();
+                hand.PalmBase = (Transform)EditorGUILayout.ObjectField("Palm Base", hand.PalmBase, typeof(Transform), allowSceneObjects: true);
+            }
+
+            showAdvConfig = EditorGUILayout.Foldout(showAdvConfig, showAdvConfigTxt);
+            if (showAdvConfig) {
+                EditorGUILayout.LabelField("Optional", EditorStyles.boldLabel);
+                hand.flatnessChecker = (FlatnessChecker) EditorGUILayout.ObjectField("Flatness Checker", hand.flatnessChecker, typeof(FlatnessChecker), allowSceneObjects:true);
+                hand.objectLayer = EditorGUILayout.LayerField("Object Layer", hand.objectLayer);
+                EditorGUILayout.Space();
+
+                hand.palmMeshWait = EditorGUILayout.FloatField("Palm Mesh Generation Tick", hand.palmMeshWait);
+                EditorGUILayout.Space();
+
+                EditorGUILayout.LabelField("Finger Collider Reset", EditorStyles.boldLabel);
+                hand.tooClose = EditorGUILayout.FloatField("Too Close Threshold", hand.tooClose);
+                hand.tooFast = EditorGUILayout.FloatField("Too Fast Threshold", hand.tooFast);
+            }
+
+            showDebug = EditorGUILayout.Foldout(showDebug, showDebugTxt);
+            if (showDebug) {
+                hand.DestroyFingerRenderersOnSpawn = EditorGUILayout.Toggle("Destroy Finger Renderers on Spawn", hand.DestroyFingerRenderersOnSpawn);
+                hand.showPalmMesh = EditorGUILayout.Toggle("Show Palm Meshes", hand.showPalmMesh);
+                EditorGUILayout.Space();
+
+                EditorGUILayout.LabelField("Time Since Last Grab", hand.timeSinceGrabbing.ToString("F3"));
+                EditorGUILayout.LabelField("Time Since Last Release", hand.timeSinceRelease.ToString("F3"));
+                EditorGUILayout.LabelField("Time Since Last Two-Hand Grab", hand.timeSinceTwoHandGrabbing.ToString("F3"));
+                EditorGUILayout.Space();
+
+                EditorGUILayout.LabelField("F1", hand.f1.ToString());
+                EditorGUILayout.LabelField("F2", hand.f2.ToString());
+                EditorGUILayout.LabelField("Dist 1", hand.dist1.ToString("F3"));
+                EditorGUILayout.LabelField("Dist 2", hand.dist2.ToString("F3"));
+                EditorGUILayout.LabelField("Ratio 1", hand.ratio1.ToString("F3"));
+                EditorGUILayout.LabelField("Ratio 2", hand.ratio2.ToString("F3"));
+            }
+        }
+    }
+}
--- Assets/Maestro/Core/Editor/MaestroInteractableEditor.cs
+++ Assets/Maestro/Core/Editor/MaestroInteractableEditor.cs
@@ -10,12 +10,82 @@ namespace Maestro
     [CustomEditor(typeof(MaestroInteractable))]
     public class MaestroInteractableEditor : Editor
     {
+        private bool showAdvConfig = false;
+        private string showAdvConfigTxt = "Advanced Configuration";
+
+        private bool showEvents = false;
+        private string showEventsTxt = "Bind Events";
+
+        private SerializedProperty OnTouchProp, UnTouchProp, OnGrabProp, OnReleaseProp;
+
+        private void OnEnable()
+        {
+            OnTouchProp = serializedObject.FindProperty("onTouch");
+            UnTouchProp = serializedObject.FindProperty("unTouch");
+            OnGrabProp = serializedObject.FindProperty("onGrab");
+            OnReleaseProp = serializedObject.FindProperty("onRelease");
+        }
+
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
+            MaestroInteractable mi = (MaestroInteractable) target;
 
-            MaestroInteractable mi = target as MaestroInteractable;
+            /** 
+             * Haptics
+             */
+            EditorGUILayout.LabelField("Haptic Configuration", EditorStyles.boldLabel);
+
+            mi.Amplitude = (byte)EditorGUILayout.IntSlider("Amplitude", mi.Amplitude, 0, 255);
+            mi.VibrationEffect = (byte)EditorGUILayout.IntSlider("Vibration Effect", mi.VibrationEffect, 0, 128);
+            // Describe DRV2605 effect below
             EditorGUILayout.HelpBox(DRV2605Descriptions.get(mi.VibrationEffect), MessageType.None, false);
+            EditorGUILayout.Space();
+
+            /**
+             * Configuration
+             */
+            mi.type = (InteractionType) EditorGUILayout.EnumPopup("Type", mi.type);
+            mi.IgnoreTaps = EditorGUILayout.Toggle("Ignore Taps", mi.IgnoreTaps);
+            mi.isPersistent = EditorGUILayout.Toggle("Persist", mi.isPersistent);
+            if (mi.isPersistent) {
+                mi.persistanceDuration = EditorGUILayout.FloatField("Persistence Duration", mi.persistanceDuration);
+            }
+            EditorGUILayout.Space();
+
+            /**
+             * Bind events
+             */
+            showEvents = EditorGUILayout.Foldout(showEvents, showEventsTxt);
+            if (showEvents) {
+                EditorGUILayout.PropertyField(OnGrabProp);
+                EditorGUILayout.PropertyField(OnReleaseProp);
+                EditorGUILayout.Space();
+
+                EditorGUILayout.LabelField("Finger events", EditorStyles.boldLabel);
+                EditorGUILayout.PropertyField(OnTouchProp);
+                EditorGUILayout.PropertyField(UnTouchProp);
+            }
+
+            /**
+             * Advanced config
+             */
+            showAdvConfig = EditorGUILayout.Foldout(showAdvConfig, showAdvConfigTxt);
+            if (showAdvConfig) {
+                mi.UseRenderCenter = EditorGUILayout.Toggle("Use Render Center", mi.UseRenderCenter);
+                mi.SendHapticsToWholeHand = EditorGUILayout.Toggle("Send Haptics to Whole Hand", mi.SendHapticsToWholeHand);
+                EditorGUILayout.Space();
+
+                mi.maintainOrientation = EditorGUILayout.Toggle("Maintain Orientation", mi.maintainOrientation);
+                mi.maintainPosition = EditorGUILayout.Toggle("Maintain Position", mi.maintainPosition);
+                mi.stayInHand = EditorGUILayout.Toggle("Stay in hand", mi.stayInHand);
+                EditorGUILayout.Space();
+
+                mi.gripCollider = EditorGUILayout.ObjectField("Grip Collider", mi.gripCollider, typeof(Collider), allowSceneObjects: true) as Collider;
+                mi.gripTransform = EditorGUILayout.ObjectField("Grip Transform", mi.gripTransform, typeof(Transform), allowSceneObjects: true) as Transform;
+            }
+
+            // Apply property changes
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
