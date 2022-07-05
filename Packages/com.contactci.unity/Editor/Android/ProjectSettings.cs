5b46b346275242cf48e02cfa45dffb885cee0405
John Schroeder <john@contactci.co>
2022-07-05 18:37:46 +0000

    Bugfix/fix android build dep

---
+++ Packages/com.contactci.unity/Editor/Android/ProjectSettings.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+namespace ContactCi.Android
+{
+    public class ProjectSettings : ScriptableObject
+    {
+        public const string AssetFilePath = "Assets/Maestro/ProjectSettings.asset";
+        
+        [SerializeField]
+        private bool shouldPatchManifest;
+
+        public bool ShouldPatchManifest => shouldPatchManifest;
+
+        private static ProjectSettings GetDefaultSettings()
+        {
+            var settings = CreateInstance<ProjectSettings>();
+            
+            settings.shouldPatchManifest = true;
+            
+            return settings;
+        }
+
+        public static ProjectSettings GetOrCreateSettings()
+        {
+            var settings = AssetDatabase.LoadAssetAtPath<ProjectSettings>(AssetFilePath);
+
+            // No settings saved; use default
+            if (settings == null)
+            {
+                settings = GetDefaultSettings();
+
+                string assetFileFolder = Path.GetDirectoryName(AssetFilePath);
+
+                if (!AssetDatabase.IsValidFolder(assetFileFolder))
+                {
+                    Directory.CreateDirectory(assetFileFolder);
+                }
+
+                AssetDatabase.CreateAsset(settings, AssetFilePath);
+                AssetDatabase.SaveAssets();
+            }
+
+            return settings;
+        }
+        
+        internal static SerializedObject GetSerializedSettings()
+        {
+            return new SerializedObject(GetOrCreateSettings());
+        }
+    }
+}
