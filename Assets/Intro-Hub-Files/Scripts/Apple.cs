f34b69a252f8edd17047dda8ebeec5bebcb066e4
Alex McDowell <alex.mcdowell@contactci.co>
2022-10-17 13:08:01 -0600

    Fixing bloom so it's gradual

--- Assets/Intro-Hub-Files/Scripts/Apple.cs
+++ Assets/Intro-Hub-Files/Scripts/Apple.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,9 @@ public class Apple : MonoBehaviour
     public Volume postVolume;
     public Bloom bloomEffect;
 
+    private bool BloomUp;
+    private bool BloomDown;
+
     private void OnTriggerEnter(Collider other)
     {
 
@@ -22,12 +26,16 @@ public class Apple : MonoBehaviour
             if (volume.profile.TryGet<Bloom>(out bloomEffect))
             {
                 Debug.Log("Bloom");
-                bloomEffect.intensity.value = 1000f;
+                BloomUp = true;
             }
         }
         
 
     }
+
+    
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +46,29 @@ public class Apple : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(BloomUp)
+        {
+            if(bloomEffect.intensity != 1000)
+            {
+                bloomEffect.intensity.value += 1f;
+            }
+            else
+            {
+                BloomDown = true;
+                BloomUp = false;
+            }
+        }
+        else if(BloomDown)
+        {
+
+            if (bloomEffect.intensity != 0f)
+            {   
+                bloomEffect.intensity.value -= 1f;
+            }
+            else
+            {
+                Debug.Log("Done");
+            }
+        }
     }
 }
