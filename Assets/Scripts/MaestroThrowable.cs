9d5432af5e1812aef6685b3eeec569cd0e4192b0
Jonathan Ampiaw <jonathan.ampiaw@contactci.co>
2023-05-16 14:40:34 -0400

    Updated throwable for better throwing, made reset button respawn everything

--- Assets/Scripts/MaestroThrowable.cs
+++ Assets/Scripts/MaestroThrowable.cs
@@ -1,9 +1,13 @@
+using Leap.Unity;
+using Leap.Unity.HandsModule;
 using Leap.Unity.Interaction.PhysicsHands;
+using Leap.Unity.Preview.InputActions;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using static Leap.Unity.Preview.InputActions.LeapInputActionUpdater;
 
 public class MaestroThrowable : MonoBehaviour
 {
@@ -11,9 +15,14 @@ public class MaestroThrowable : MonoBehaviour
     private Rigidbody rb;
     private bool isGrasped;
     private PhysicsHand graspedHand;
-    private float extraThrowForce = 9f;
+    private HandModelBase handModelBase;
+    private float extraThrowForce = 2f;
     private float minVelocityForThrow = 1.4f;
 
+    /* Track velocity for last 4 frames 
+      If lost tracking and velocity in the last 4 frames was enough to be considered a throw, throw the object
+
+     */
     private void Start()
     {
         PhysicsProvider = FindObjectOfType<PhysicsProvider>();
@@ -25,32 +34,38 @@ public class MaestroThrowable : MonoBehaviour
     {
         PhysicsProvider.OnObjectStateChange -= CheckForThrow;
     }
-    private IEnumerator ThrowCoroutine()
+    private IEnumerator CalculateVelocity()
     {
         List<Vector3> velocitySamples = new List<Vector3>();
-        for (int i=0; i < 8; i++)
+        Vector3 averageVelocity = Vector3.zero;
+        for (int i = 0; i < 8; i++)
         {
             velocitySamples.Add(rb.velocity);
-            yield return null;
+            yield return new WaitForFixedUpdate();
         }
         yield return new WaitForSeconds(0.04f);
-        Vector3 averageVelocity = new Vector3();
-        foreach(Vector3 velocity in velocitySamples)
+        foreach (Vector3 velocity in velocitySamples)
         {
             averageVelocity += velocity;
         }
         averageVelocity = averageVelocity / velocitySamples.Count;
         if (averageVelocity.magnitude > minVelocityForThrow)
         {
-            rb.AddForce(averageVelocity.normalized * 2f, ForceMode.Impulse);
+            Throw(averageVelocity);
         }
+    }
+
+    private void Throw(Vector3 averageVelocity)
+    {
+        rb.AddForce(averageVelocity.normalized * extraThrowForce, ForceMode.Impulse);
     }
     private void CheckForThrow(Rigidbody arg1, PhysicsGraspHelper arg2)
     {
+
         if (isGrasped && PhysicsProvider.IsGraspingObject(rb) == false)
         {
             isGrasped = false;
-            StartCoroutine(ThrowCoroutine());
+            StartCoroutine(CalculateVelocity());
             graspedHand = null;
         }
 
@@ -58,7 +73,13 @@ public class MaestroThrowable : MonoBehaviour
         {
             isGrasped = true;
             graspedHand = PhysicsProvider.LeftHand.IsGrasping == true ? PhysicsProvider.LeftHand : PhysicsProvider.RightHand;
+            handModelBase = FindObjectsOfType<HandBinder>().First(o => o.Handedness == graspedHand.Handedness);
         }
+
+    }
+
+    private void FixedUpdate()
+    {
 
     }
 }
