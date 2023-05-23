b0e5a57855ee7c5ffbfb88746152c7a8dd321a37
Jonathan Ampiaw <jonathan.ampiaw@contactci.co>
2023-05-23 13:15:53 -0400

    Cleaned up throwing, added constraints for articulationBodies to reduce spams while pushing against immovable objects

---
+++ Assets/Scripts/ArticulationBodyConstraint.cs
@@ -0,0 +1,184 @@
+using Leap;
+using Leap.Unity.Interaction.PhysicsHands;
+using Maestro;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using UnityEngine;
+
+public class ArticulationBodyData
+{
+    public float maxAngularVelocity;
+    public float maxDepenetrationVelocity;
+    public float maxJointVelocity;
+    public float maxLinearVelocity;
+    public ArticulationDrive xDrive;
+    public float angularDamping;
+    public float mass;
+    public float jointFriction;
+
+    public ArticulationBodyData(ArticulationBody body)
+    {
+        maxAngularVelocity = body.maxAngularVelocity;
+        maxDepenetrationVelocity = body.maxDepenetrationVelocity;
+        maxJointVelocity = body.maxJointVelocity;
+        maxLinearVelocity = body.maxJointVelocity;
+        xDrive = body.xDrive;
+        angularDamping = body.angularDamping;
+        mass = body.mass;
+        jointFriction = body.jointFriction;
+    }
+}
+public class ArticulationBodyConstraint : MonoBehaviour
+{
+    private PhysicsHand physicsHand;
+    private List<ArticulationBody> bodies = new List<ArticulationBody>();
+    private Dictionary<ArticulationBody, ArticulationBodyData> initialBodyVelocities = new Dictionary<ArticulationBody, ArticulationBodyData>();
+    private float initialMaximumPalmVelocity;
+    private float initialMaxmimumFingerVelocity;
+    private PhysicsHand.Hand hand;
+    private void Awake()
+    {
+        physicsHand = GetComponent<PhysicsHand>();
+        hand = physicsHand.GetPhysicsHand();
+        foreach (PhysicsBone bone in hand.jointBones)
+        {
+            initialBodyVelocities.Add(bone.ArticulationBody, new ArticulationBodyData(bone.ArticulationBody));
+            bone.ArticulationBody.maxAngularVelocity = 0.1f;
+        }
+        initialBodyVelocities.Add(hand.palmBone.ArticulationBody, new ArticulationBodyData(hand.palmBone.ArticulationBody));
+        initialMaximumPalmVelocity = hand.maximumPalmVelocity;
+        initialMaxmimumFingerVelocity = hand.maximumFingerVelocity;
+    }
+    private bool IsAnyBoneContacting()
+    {
+        foreach (PhysicsBone bone in hand.jointBones)
+        {
+            List<Collider> correctColliders = bone.ContactingObjects.Where(o => o.GetComponent<FingerCollider>() == null && o.GetComponent<Collider>() != null).Select(c => c.GetComponent<Collider>()).ToList();
+            if (bone.IsContacting && correctColliders.Count > 0)
+            {
+                return true;
+            }
+        }
+            return false;
+    }
+
+    private bool IsPalmContacting()
+    {
+        PhysicsBone palmBone = hand.palmBone;
+        List<Rigidbody> palmRigidbodies = palmBone.ContactingObjects.Where(o => o.GetComponent<FingerCollider>() == null).ToList();
+        return palmBone.IsContacting && palmRigidbodies.Count > 0;
+    }
+
+    private void Update()
+    {
+        ConstrainHand();
+    }
+
+    private void ConstrainHand()
+    {
+        if(IsAnyBoneContacting() || IsPalmContacting() && !physicsHand.IsGrasping)
+        {
+            hand.maximumPalmVelocity = 0.05f;
+            hand.maximumPalmAngularVelocity = 2f;
+        }
+        else
+        {
+            hand.maximumPalmVelocity = initialMaximumPalmVelocity;
+            hand.maximumFingerVelocity = initialMaxmimumFingerVelocity;
+            hand.maximumPalmAngularVelocity = 6000f;
+        }
+        foreach (PhysicsBone bone in hand.jointBones)
+        {
+            ConstrainFinger(bone);
+        }
+        PhysicsBone palmBone = hand.palmBone;
+        List<Rigidbody> palmRigidbodies = palmBone.ContactingObjects.Where(o => o.GetComponent<FingerCollider>() == null).ToList();
+        if (palmBone.IsContacting && palmRigidbodies.Count > 0)
+        {
+            hand.maximumPalmVelocity = 0.05f;
+            palmBone.ArticulationBody.jointFriction = 10000f;
+            palmBone.ArticulationBody.velocity = Vector3.zero;
+            palmBone.ArticulationBody.angularVelocity = Vector3.zero;
+            palmBone.ArticulationBody.maxAngularVelocity = 0.0f;
+            palmBone.ArticulationBody.maxDepenetrationVelocity = 0.0f;
+            palmBone.ArticulationBody.maxJointVelocity = 0.0f;
+            palmBone.ArticulationBody.maxLinearVelocity = 0.0f;
+            palmBone.ArticulationBody.angularDamping = 500f;
+        }
+        else
+        {
+            hand.maximumPalmVelocity = initialMaximumPalmVelocity;
+            hand.maximumPalmAngularVelocity = 6000f;
+            ArticulationBodyData data = initialBodyVelocities[palmBone.ArticulationBody];
+            palmBone.ArticulationBody.maxAngularVelocity = data.maxAngularVelocity;
+            palmBone.ArticulationBody.maxDepenetrationVelocity = data.maxDepenetrationVelocity;
+            palmBone.ArticulationBody.maxJointVelocity = data.maxJointVelocity;
+            palmBone.ArticulationBody.maxLinearVelocity = data.maxLinearVelocity;
+            palmBone.ArticulationBody.xDrive = data.xDrive;
+            palmBone.ArticulationBody.angularDamping = data.angularDamping;
+            palmBone.ArticulationBody.mass = data.mass;
+        }
+    }
+    private void ConstrainFinger(PhysicsBone bone)
+    {
+        List<Collider> correctColliders = bone.ContactingObjects.Where(o => o.GetComponent<FingerCollider>() == null && o.GetComponent<Collider>() != null).Select(c => c.GetComponent<Collider>()).ToList();
+        if (bone.IsContacting && correctColliders.Count > 0)
+        {
+            bone.ArticulationBody.maxAngularVelocity = 0.01f;
+            bone.ArticulationBody.maxDepenetrationVelocity = 0.01f;
+            bone.ArticulationBody.maxJointVelocity = 0.01f;
+            bone.ArticulationBody.maxLinearVelocity = 0.01f;
+            bone.ArticulationBody.jointFriction = 1000f;
+            bone.ArticulationBody.xDrive = new ArticulationDrive
+            {
+                forceLimit = 0.01f,
+                stiffness = 10000f,
+                damping = bone.ArticulationBody.xDrive.damping,
+                lowerLimit = -10f,
+                upperLimit = 10f,
+                target = bone.ArticulationBody.xDrive.target,
+                targetVelocity = bone.ArticulationBody.xDrive.targetVelocity
+            };
+            //if (bone.ArticulationBody.jointType == ArticulationJointType.SphericalJoint)
+            //{
+            //    bone.ArticulationBody.yDrive = new ArticulationDrive
+            //    {
+            //        forceLimit = 0.01f,
+            //        stiffness = 10000f,
+            //        damping = 25f,
+            //        lowerLimit = -10f,
+            //        upperLimit = 10f,
+            //        target = bone.ArticulationBody.yDrive.target,
+            //        targetVelocity = bone.ArticulationBody.yDrive.targetVelocity
+            //    };
+            //    bone.ArticulationBody.zDrive = new ArticulationDrive
+            //    {
+            //        forceLimit = 0.01f,
+            //        stiffness = 10000f,
+            //        damping = bone.ArticulationBody.zDrive.damping,
+            //        lowerLimit = 0f,
+            //        upperLimit = 0f,
+            //        target = bone.ArticulationBody.zDrive.target,
+            //        targetVelocity = bone.ArticulationBody.zDrive.targetVelocity
+            //    };
+            //}
+            bone.ArticulationBody.velocity = Vector3.zero;
+            bone.ArticulationBody.angularVelocity = Vector3.zero;
+        }
+        else
+        {
+            ArticulationBodyData data = initialBodyVelocities[bone.ArticulationBody];
+            bone.ArticulationBody.maxAngularVelocity = data.maxAngularVelocity;
+            bone.ArticulationBody.maxDepenetrationVelocity = data.maxDepenetrationVelocity;
+            bone.ArticulationBody.maxJointVelocity = data.maxJointVelocity;
+            bone.ArticulationBody.maxLinearVelocity = data.maxLinearVelocity;
+            bone.ArticulationBody.xDrive = data.xDrive;
+            bone.ArticulationBody.mass = data.mass;
+            bone.ArticulationBody.jointFriction = data.jointFriction;
+        }
+    }
+
+}
--- Assets/Scripts/MaestroThrowable.cs
+++ Assets/Scripts/MaestroThrowable.cs
@@ -1,13 +1,11 @@
 using Leap.Unity;
 using Leap.Unity.HandsModule;
 using Leap.Unity.Interaction.PhysicsHands;
-using Leap.Unity.Preview.InputActions;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using static Leap.Unity.Preview.InputActions.LeapInputActionUpdater;
 
 public class MaestroThrowable : MonoBehaviour
 {
@@ -15,71 +13,81 @@ public class MaestroThrowable : MonoBehaviour
     private Rigidbody rb;
     private bool isGrasped;
     private PhysicsHand graspedHand;
+    private Transform palmTransform;
     private HandModelBase handModelBase;
     private float extraThrowForce = 2f;
     private float minVelocityForThrow = 1.4f;
+    private bool isLocatingHand;
 
-    /* Track velocity for last 4 frames 
-      If lost tracking and velocity in the last 4 frames was enough to be considered a throw, throw the object
-
-     */
     private void Start()
     {
-        PhysicsProvider = FindObjectOfType<PhysicsProvider>();
-        rb = GetComponent<Rigidbody>();
-        PhysicsProvider.OnObjectStateChange += CheckForThrow;
+        this.PhysicsProvider = UnityEngine.Object.FindObjectOfType<PhysicsProvider>();
+        this.rb = this.GetComponent<Rigidbody>();
+        this.PhysicsProvider.OnObjectStateChange += new Action<Rigidbody, PhysicsGraspHelper>(this.DetectGrabOrThrow);
     }
 
-    private void OnDestroy()
-    {
-        PhysicsProvider.OnObjectStateChange -= CheckForThrow;
-    }
-    private IEnumerator CalculateVelocity()
+    private void OnDestroy() => this.PhysicsProvider.OnObjectStateChange -= new Action<Rigidbody, PhysicsGraspHelper>(this.DetectGrabOrThrow);
+
+    private IEnumerator ThrowAveragedVelocity()
     {
         List<Vector3> velocitySamples = new List<Vector3>();
         Vector3 averageVelocity = Vector3.zero;
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 8; ++i)
         {
-            velocitySamples.Add(rb.velocity);
-            yield return new WaitForFixedUpdate();
+            velocitySamples.Add(this.rb.velocity);
+            yield return (object)new WaitForFixedUpdate();
         }
-        yield return new WaitForSeconds(0.04f);
-        foreach (Vector3 velocity in velocitySamples)
-        {
-            averageVelocity += velocity;
-        }
-        averageVelocity = averageVelocity / velocitySamples.Count;
-        if (averageVelocity.magnitude > minVelocityForThrow)
-        {
-            Throw(averageVelocity);
-        }
+        yield return (object)new WaitForSeconds(0.04f);
+        foreach (Vector3 vector3 in velocitySamples)
+            averageVelocity += vector3;
+        averageVelocity /= (float)velocitySamples.Count;
+        if ((double)averageVelocity.magnitude > (double)this.minVelocityForThrow)
+            this.Throw(averageVelocity);
     }
 
-    private void Throw(Vector3 averageVelocity)
+    private void Throw(Vector3 averageVelocity) => this.rb.AddForce(averageVelocity * this.extraThrowForce, ForceMode.Impulse);
+
+    private void DetectGrabOrThrow(Rigidbody arg1, PhysicsGraspHelper arg2)
     {
-        rb.AddForce(averageVelocity.normalized * extraThrowForce, ForceMode.Impulse);
-    }
-    private void CheckForThrow(Rigidbody arg1, PhysicsGraspHelper arg2)
-    {
-
-        if (isGrasped && PhysicsProvider.IsGraspingObject(rb) == false)
+        if (this.isGrasped && !this.PhysicsProvider.IsGraspingObject(this.rb))
         {
-            isGrasped = false;
-            StartCoroutine(CalculateVelocity());
-            graspedHand = null;
+            this.isGrasped = false;
+            this.StartCoroutine(this.ThrowAveragedVelocity());
+            this.graspedHand = (PhysicsHand)null;
         }
-
-        if (arg1 == rb && arg2.GraspState == PhysicsGraspHelper.State.Grasp)
-        {
-            isGrasped = true;
-            graspedHand = PhysicsProvider.LeftHand.IsGrasping == true ? PhysicsProvider.LeftHand : PhysicsProvider.RightHand;
-            handModelBase = FindObjectsOfType<HandBinder>().First(o => o.Handedness == graspedHand.Handedness);
-        }
-
+        if (!((UnityEngine.Object)arg1 == (UnityEngine.Object)this.rb) || arg2.GraspState != PhysicsGraspHelper.State.Grasp)
+            return;
+        this.isGrasped = true;
+        this.graspedHand = this.PhysicsProvider.LeftHand.IsGrasping ? this.PhysicsProvider.LeftHand : this.PhysicsProvider.RightHand;
+        this.handModelBase = (HandModelBase)((IEnumerable<HandBinder>)UnityEngine.Object.FindObjectsOfType<HandBinder>()).First<HandBinder>((Func<HandBinder, bool>)(o => o.Handedness == this.graspedHand.Handedness));
+        this.palmTransform = this.graspedHand.GetComponentsInChildren<Transform>(true)[1];
     }
 
     private void FixedUpdate()
     {
+    }
 
+    private void Update()
+    {
+        if (!((UnityEngine.Object)this.handModelBase != (UnityEngine.Object)null) || this.isLocatingHand || !this.isGrasped || this.handModelBase.IsTracked)
+            return;
+        this.isLocatingHand = true;
+        this.StartCoroutine(this.LocatingHand());
+    }
+
+    private IEnumerator LocatingHand()
+    {
+        MaestroThrowable maestroThrowable = this;
+        Vector3 position = maestroThrowable.handModelBase.transform.position;
+        Vector3 vector3 = maestroThrowable.handModelBase.transform.position - maestroThrowable.transform.position;
+        Vector3 throwDirection = maestroThrowable.palmTransform.transform.forward;
+        while (!maestroThrowable.handModelBase.IsTracked)
+            yield return (object)null;
+        Vector3 newForwardDirection = maestroThrowable.palmTransform.transform.forward;
+        yield return (object)null;
+        yield return (object)null;
+        if ((double)Vector3.Angle(throwDirection, newForwardDirection) > 50.0 && !maestroThrowable.PhysicsProvider.IsGraspingObject(maestroThrowable.rb))
+            maestroThrowable.Throw(newForwardDirection * 2f);
+        maestroThrowable.isLocatingHand = false;
     }
 }
