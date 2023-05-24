a5b49fd272165b9c00839df09ccc7d3ffe6210cc
Alex McDowell <alex.mcdowell@contactci.co>
2023-05-24 11:28:12 -0600

    Script adjusts objects to set percentage of player height

--- Assets/Scripts/VRHeightAdjustment.cs
+++ Assets/Scripts/VRHeightAdjustment.cs
@@ -1,4 +1,5 @@
 using Leap.Unity;
+using Maestro;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,13 +10,23 @@ public class VRHeightAdjustment : MonoBehaviour
     // Start is called before the first frame update
 
     [SerializeField] GameObject rigTransform;
+    [SerializeField] GameObject demoTable;
 
+    //Remove Later
+    //Used for Visual Debugging
     [SerializeField] float maxY;
-        
+    [SerializeField] float minY = 0;
+    [SerializeField] float demoTableY;
 
+    [Range(0.0f, 100f)]
+    [SerializeField] int heightPercentage;
 
     void Start()
     {
+        Vector3 startPos = demoTable.transform.position;
+        startPos.y = 0;
+        demoTable.transform.position = startPos;
+
         maxY = rigTransform.transform.position.y;
     }
 
@@ -25,6 +36,22 @@ public class VRHeightAdjustment : MonoBehaviour
         if(maxY < rigTransform.transform.position.y)
         {
             maxY = rigTransform.transform.position.y;
+            AdjustSceneHeight();
         }
+    }
+
+    public void AdjustSceneHeight()
+    {
+        Vector3 tempPos = demoTable.transform.position;
+        tempPos.y = maxY * ((float)heightPercentage / 100f);
+        Debug.Log(tempPos.y);
+        demoTable.transform.position = tempPos;
+        demoTableY = tempPos.y;
+    }
+
+
+    public void AdjustPlayerHeight()
+    {
+        
     }
 }
