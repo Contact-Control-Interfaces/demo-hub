70273f5688988fb6022e2d0db3007b543c056ae1
Zach Schroeder <zach@contactci.co>
2023-05-26 18:33:08 -0400

    Remove unused usings that stopped building

--- Assets/Scripts/VRHeightAdjustment.cs
+++ Assets/Scripts/VRHeightAdjustment.cs
@@ -1,10 +1,4 @@
-using Leap.Unity;
-using Maestro;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
-using UnityEngine.XR;
 
 public class VRHeightAdjustment : MonoBehaviour
 {
