8a7b5832cbacc5f18dc06f990b92bfb7e9afc972
Zach Schroeder <zach@contactci.co>
2024-04-19 13:29:02 -0400

    Save initial rotation, reset skybox material rotation on app quit

--- Assets/Scripts/Skybox/RotateSky.cs
+++ Assets/Scripts/Skybox/RotateSky.cs
@@ -1,17 +1,34 @@
 using System.Collections;
-using System.Collections.Generic; 
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class RotateSky : MonoBehaviour
 {
     [Header("Rotation Speed")]
     public float rotateSpeed;
-    //public Skybox skybox;
+
+    private const string RotationFieldName = "_Rotation";
+    private float InitialRotation;
+
+    private void Start()
+    {
+        float initialRotation = RenderSettings.skybox.GetFloat(RotationFieldName);
+    }
 
     private void Update()
     {
-       
-        RenderSettings.skybox.SetFloat("_Rotation", Time.time * rotateSpeed);
-        //Debug.Log(RenderSettings.skybox.GetFloat("_Rotation"));
+        SetSkyboxRotation(Time.time * rotateSpeed);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SetSkyboxRotation(InitialRotation);
+    }
+
+    private void SetSkyboxRotation(float rotation)
+    {
+        RenderSettings.skybox.SetFloat(RotationFieldName, rotation);
     }
 }
