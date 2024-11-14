141a6b3ec0197da763faff5c57071718d5ff7fee
Alex McDowell <alex.mcdowell@contactci.co>
2024-11-14 14:10:33 -0700

    Creating script for activation panel to recognize when all switches are on

---
+++ Assets/Scripts/ActivatePanel.cs
@@ -0,0 +1,69 @@
+using Maestro;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ActivatePanel : MonoBehaviour
+{
+
+    public List<GameObject> panelButtons= new List<GameObject>();
+    public List<bool> active = new List<bool>();
+    public ConveyorBelt conveyorBelt;
+
+    public void Start()
+    {
+        ToggleCheck();
+    }
+
+    public void ToggleCheck()
+    {
+        foreach(GameObject go in panelButtons)
+        {
+            if(go.GetComponent<ToggleSwitchBehavior>() != null)
+            {
+                ToggleSwitchBehavior currentToggle = go.GetComponent<ToggleSwitchBehavior>();
+                int index = panelButtons.IndexOf(go);
+                active[index] = (currentToggle.state == ToggleState.On) ? true: false;
+
+            }
+
+            else if(go.GetComponent<IndustrialSwitchBehavior>() != null)
+            {
+                IndustrialSwitchBehavior currentToggle = go.GetComponent<IndustrialSwitchBehavior>();
+                int index = panelButtons.IndexOf(go);
+                active[index] = (currentToggle.state == IndustrialToggleState.On) ? true : false;
+            }
+        }
+
+        Debug.Log( "All on?: " + AllActiveCheckDebug());
+    }
+
+    public void OnAllSwitchesToggled()
+    {
+        conveyorBelt.ActivateBelt();
+    }
+
+    public bool AllActiveCheckDebug()
+    {
+        string resultString = "";
+
+        foreach (bool booleanVal in active)
+        {
+            resultString = resultString + " " + booleanVal;
+        }
+
+        if (active.All(currentBool => currentBool == true))
+        {
+            Debug.Log(resultString);
+            return true;
+        }
+        else
+        {
+            Debug.Log(resultString);
+            return false;
+        }
+            //OnAllSwitchesToggled();
+    }
+}
---
+++ Assets/Scripts/Conveyor/ConveyorBelt.cs
@@ -0,0 +1,30 @@
+using Autodesk.Fbx;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    List<Belt> beltList = new List<Belt>();
+
+    void Start()
+    {
+        foreach(Belt belt in beltList)
+        {
+            belt.enabled = false;
+        }
+    }
+
+    public void ActivateBelt()
+    {
+        foreach(Belt belt in beltList)
+        {
+            belt.enabled = true;
+        }
+
+        beltList.Last<Belt>().endPoint = true;
+    }
+}
