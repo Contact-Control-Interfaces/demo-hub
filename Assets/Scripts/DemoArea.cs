1c5b5aa2e33387145772ba63091882eeff1c785f
Alex McDowell <alex.mcdowell@contactci.co>
2025-01-03 12:02:27 -0600

    Creating base logic for spawn reset script

--- Assets/Scripts/DemoArea.cs
+++ Assets/Scripts/DemoArea.cs
@@ -1,28 +1,46 @@
 using Maestro;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class DemoArea : MonoBehaviour
 {
     public GameObject demoArea;
+    public GameObject alertBox;
     public MaestroManager playerRig;
 
-    public void OnTriggerExit(Collider other)
+
+    float elapsedTime;
+    public float waitLength;
+
+
+
+public void OnTriggerExit(Collider other)
+{
+   if(other.gameObject.GetComponent<MaestroManager>())
     {
-       if(other.gameObject.GetComponent<MaestroManager>())
-        {
 
-        }
-    }
-
-    public void outofBoundsAlert()
-    {
-
-    }
-
-    public void Reset()
-    {
-        
     }
 }
+
+public void outofBoundsAlert()
+{
+
+}
+
+private IEnumerator AlertCountDown()
+{
+    do
+    {
+        yield return new WaitForSeconds(.01f);
+    }
+    while (elapsedTime < waitLength);
+    resetLocation();
+}
+
+public void resetLocation()
+{
+
+}
+
